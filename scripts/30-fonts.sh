1990a00083eabf4d12b076fe9a6925010d2d5e67
Kenton <kappanaitis@gmail.com>
2022-11-05 14:32:53 -0500

    Creating temp fonts dir

--- scripts/30-fonts.sh
+++ scripts/30-fonts.sh
@@ -10,20 +10,22 @@ if [[ $SKIP_FONTS ]]; then
     exit 0
 fi
 
+mkdir -p ${TMPDIR}/fonts
+
 FONTSDIR=${HOME}/.local/share/fonts
-echo "Cleaning ${FONTSDIR}"
+echo -e "\nCleaning ${FONTSDIR}"
 rm -rf ${FONTSDIR} && mkdir -p ${FONTSDIR}
 
-echo "Installing JetBrains Mono Fonts"
+echo -e "\nInstalling JetBrains Mono Fonts"
 git clone --depth 1 -- https://github.com/JetBrains/JetBrainsMono.git ${CODEDIR}/public/JetBrainsMono
 ln -s ${CODEDIR}/public/JetBrainsMono $FONTSDIR/JetBrainsMono
 
-echo "Installing Source Sans Pro Fonts"
+echo -e "\nInstalling Source Sans Pro Fonts"
 mkdir -p ${FONTSDIR}/SourceSansPro
-curl -o source_sans_pro.zip --output-dir ${TMPDIR} https://fonts.google.com/download?family=Source%20Sans%20Pro
-unzip ${TMPDIR}/source_sans_pro.zip -d ${FONTSDIR}/SourceSansPro
+curl -o source_sans_pro.zip --output-dir ${TMPDIR}/fonts/ https://fonts.google.com/download?family=Source%20Sans%20Pro
+unzip ${TMPDIR}/fonts/source_sans_pro.zip -d ${FONTSDIR}/SourceSansPro
 
-echo "Installing Powerline Fonts"
+echo -e "\nInstalling Powerline Fonts"
 if [[ "$OSNAME" = "Fedora" ]]; then
     sudo dnf install powerline powerline-fonts
 fi
@@ -32,10 +34,10 @@ if [[ "$OSNAME" = "Ubuntu" ]] || [[ "$OSNAME" = "Pop!_OS" ]]; then
     sudo apt install fonts-powerline
 fi
 
-echo "Installing All-the-Icons Fonts"
+echo -e "\nInstalling All-the-Icons Fonts"
 git clone --depth 1 -- https://github.com/domtronn/all-the-icons.el.git ${CODEDIR}/public/all-the-icons
 ln -s ${CODEDIR}/public/all-the-icons $FONTSDIR/all-the-icons
 
-echo "Ensuring permissions for fonts are correct"
+echo -e "\nEnsuring permissions for fonts are correct"
 chown -R ${USER}:${USER} ${FONTSDIR}
 chmod -R 0755 ${FONTSDIR}
