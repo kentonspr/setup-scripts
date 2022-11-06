2cc79a17c97ba7092a19a4e14896eeba2c5f9f09
Kenton <kappanaitis@gmail.com>
2022-11-06 11:48:05 -0600

    More testing

--- scripts/workstation/13-nitrokey-luks.sh
+++ scripts/workstation/13-nitrokey-luks.sh
@@ -7,7 +7,7 @@ fi
 
 OSNAME=$(cat /etc/os-release | sed -En "s/^NAME=\"(.*)\"/\1/p")
 SCRIPT_URL="https://raw.githubusercontent.com/daringer/smartcard-key-luks/main/smartcard-key-luks"
-GPG_PIN=$(sops -d --extract '["nk_user_pin"]' ${FILESDIR}/all/vault.sops.yml)
+# GPG_PIN=$(sops -d --extract '["nk_user_pin"]' ${FILESDIR}/all/vault.sops.yml)
 
 if [[ $OSNAME = "Fedora Linux" ]]; then
     echo "Ensuring installed dependencies"
@@ -25,14 +25,11 @@ echo "Found LUKS device ${LUKS}"
 echo "Retrieving smartcard LUKS script"
 curl -O --output-dir ${TMPDIR} ${SCRIPT_URL}
 
-echo "Modifying script to accept programmatic PIN"
+# echo "Modifying script to accept programmatic PIN"
 
-sed -i '/for pb in $PUBKEYS/i eval $(gpg-agent --homedir ${GNUPGHOME} --daemon --allow-loopback-pinentry)' ${TMPDIR}/smartcard-key-luks
+# sed -i '/for pb in $PUBKEYS/i eval $(gpg-agent --homedir ${GNUPGHOME} --daemon --allow-loopback-pinentry)' ${TMPDIR}/smartcard-key-luks
 
-SEARCH='  gpg2 --homedir ${GNUPGHOME} --trust-model=always -o ${CRYPTHOME}/cryptkey.gpg $GPG_RECIPIENT --yes --encrypt ${TMPKEY}'
-REPLACE='  gpg2 --homedir ${GNUPGHOME} --trust-model=always --pinentry-mode=loopback --passphrase=${GPG_PIN} -o ${CRYPTHOME}/cryptkey.gpg $GPG_RECIPIENT --yes --encrypt ${TMPKEY}'
-
-sed -i 's#  gpg2 --homedir ${GNUPGHOME} --trust-model=always -o ${CRYPTHOME}/cryptkey.gpg $GPG_RECIPIENT --yes --encrypt ${TMPKEY}#  gpg2 --homedir ${GNUPGHOME} --trust-model=always --pinentry-mode=loopback --passphrase=${GPG_PIN} -o ${CRYPTHOME}/cryptkey.gpg $GPG_RECIPIENT --yes --encrypt ${TMPKEY}#g' ${TMPDIR}/smartcard-key-luks
+# sed -i 's#  gpg2 --homedir ${GNUPGHOME} --trust-model=always -o ${CRYPTHOME}/cryptkey.gpg $GPG_RECIPIENT --yes --encrypt ${TMPKEY}#  gpg2 --homedir ${GNUPGHOME} --trust-model=always --pinentry-mode=loopback --passphrase=${GPG_PIN} -o ${CRYPTHOME}/cryptkey.gpg $GPG_RECIPIENT --yes --encrypt ${TMPKEY}#g' ${TMPDIR}/smartcard-key-luks
 
 chmod +x ${TMPDIR}/smartcard-key-luks
 sudo -E ${TMPDIR}/smartcard-key-luks ${LUKS} ${FILESDIR}/pgp_keys/${PGP_PUBKEY_FILE}
