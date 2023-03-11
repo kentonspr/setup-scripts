5412098fab613ce86788a8322d1a0d8da2a77efd
Kenton Appanaitis <kappanaitis@gmail.com>
2023-03-11 10:23:08 -0600

    router start

---
+++ scripts/router/40-router.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+OSNAME=$(cat /etc/os-release | sed -En "s/^NAME=\"(.*)\"/\1/p")
+
+# Install packages
+sudo apt install dhcp-helper vlan
+
+# Netplan
+sudo rm --interactive=NEVER /etc/netplan/*
+sudo cp ${FILESDIR}/router/01-netcfg.yml /etc/netplan/01-netcfg.yml
+sudo netplan apply
+
+# iptables
+sudo mkdir /etc/iptables
+sudo cp ${FILESDIR}/router/iptables.rules /etc/iptables/iptables.rules
+sudo cp ${FILESDIR}/router/iptables-rules.service /etc/systemd/system/iptables-rules.service
+sudo systemctl enable iptables-rules.service
+
+# dhcp-helper
+sudo cp ${FILESDIR}/router/dhcp-helper /etc/default/dhcp-helper
+
+# Enable IP routing
+sudo -w sysctl net.ipv4.ip_forward=1
