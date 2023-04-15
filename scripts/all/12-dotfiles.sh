ccaa5ee24546c36216d2ad8c3d97a6578a934dc8
Kenton <kappanaitis@gmail.com>
2023-04-15 13:08:55 -0500

    Fixed dotfiles to use https

--- scripts/all/12-dotfiles.sh
+++ scripts/all/12-dotfiles.sh
@@ -7,4 +7,4 @@ OSNAME=$(cat /etc/os-release | sed -En "s/^NAME=\"(.*)\"/\1/p")
 
 echo -e "\n--- Clone dotfiles ---\n"
 cd ${CODEDIR}/personal
-git clone git@github.com:kentonspr/dotfiles.git
+git clone https://github.com/kentonspr/dotfiles.git
