2bcdaba36cb82f779651e472c92ae29e7d1b6313
Kenton Appanaitis <kappanaitis@gmail.com>
2023-08-14 08:30:03 -0500

    Updates

--- scripts/jollyroger/40-jollyroger.sh
+++ scripts/jollyroger/40-jollyroger.sh
@@ -7,9 +7,39 @@ if [[ $OSNAME = "Ubuntu" ]] || [[ $OSNAME = "Pop!_OS" ]]; then
     sudo apt install -y nfs-common
 fi
 
-echo -e "\n--- Add mounts ---\n"
+sudo mkdir /opt/sabnzbd_config
+# mkdir /opt/scratch
 
-sudo mkdir /opt/scratch
+echo -e "\n--- create the sabnzbd pod ---\n"
 
-echo '# User Added #' | sudo tee -a /etc/fstab
-echo 'scratch-share:/opt/scratch /opt/scratch nfs defaults 0 0' | sudo tee -a /etc/fstab
+sudo podman pod create --name sabnzbd \
+  -p 10.157.200.1:8080:8080 \
+
+sudo podman run -d \
+     --name=sabnzbd-svc \
+     --pod=sabnzbd \
+     -e PUID=1000 \
+     -e PGID=1000 \
+     -e TZ=UTC \
+     -e VERSION=docker \
+     -v /opt/sabnzbd_config:/config \
+     -v /opt/scratch:/downloads \
+     --restart unless-stopped \
+     lscr.io/linuxserver/sabnzbd:latest
+
+echo -e "\n--- generate and install systemd files ---\n"
+
+mkdir -p ${HOME}/sabnzbd
+cd ${HOME}/sabnzbd
+
+sudo podman generate systemd --new --files --name sabnzbd
+
+for SERVICE in $(find ./ -type f -name '*.service'|sort); do
+    FILENAME=$(basename -- "${SERVICE}")
+    sudo cp ${SERVICE} /etc/systemd/system/
+    sudo systemctl enable ${FILENAME}
+done
+
+sudo cp ${FILESDIR}/jollyroger/etc/cron.daily/sabnzbd-update /etc/cron.daily/sabnzbd-update
+sudo chown root:root /etc/cron.daily/sabnzbd-update
+sudo chmod 755 /etc/cron.daily/sabnzbd-update
--- scripts/workstation/70-terraform.sh
+++ scripts/workstation/70-terraform.sh
@@ -12,7 +12,7 @@ echo -e "\n--- installing tfenv ---\n"
 git clone --depth=1 https://github.com/tfutils/tfenv.git ${TFENV_DIR}
 
 [ ! -d ${HOME}/.local/bin] && mkdir -p ${HOME}/.local/bin
-ln -s ${TFENV_DIR}/* ${HOME}/.local/bin/
+ln -s ${TFENV_DIR}/bin/* ${HOME}/.local/bin/
 
 echo "Symlinking config files"
 ln -s ${CODEDIR}/personal/dotfiles/zsh/zshrc.d/80-terraform.zsh ${ZPROFILEDIR}/80-terraform.zsh
